78b141f35a9f11c7c8f1091d6942e25edd37ab4d
Dominik <rapp.dominik@hotmail.com>
2026-08-06 15:14:15 +0200

    feat: add messenger page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { WorkspaceMenu } from './components/workspace-menu/workspace-menu';
 import { ChatPanel } from './components/chat-panel/chat-panel';
 import { Imprint } from './pages/legal/imprint/imprint';
 import { PrivacyPolicy } from './pages/legal/privacy-policy/privacy-policy';
+import { Messenger } from './pages/messenger/messenger';
 
 export const routes: Routes = [
     { path: 'WorkspaceMenu', component: WorkspaceMenu },
     { path: 'imprint', component: Imprint },
     { path: 'privacy-policy', component: PrivacyPolicy },
+    { path: 'messenger', component: Messenger },
 ];
---
+++ src/app/pages/messenger/messenger.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../../components/header/header';
+
+@Component({
+  selector: 'app-messenger',
+  imports: [Header],
+  templateUrl: './messenger.html',
+  styleUrl: './messenger.scss',
+})
+export class Messenger {
+
+}
